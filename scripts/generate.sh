d951974db823df4aff31d5022bdfdc3b9f85cd41
ds-ai96 <jener05458@gmail.com>
2025-11-11 17:00:12 +0900

    Data generation (SMI & DMI)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,10 +1,10 @@
 gpu=$1
-
+mode=$2
 
 for it in $(seq 100 100 4000); do
 python data_generation.py \
     --gpu "$gpu" \
-    --mode DMI \
+    --mode "$mode" \
     --dataset /home/mjatwk/data/imagenet/ \
     --datapool /home/jener05458/src/EdgeMI/TBD_MI/dataset/ \
     --iterations "$it"
