51b512340c1c23bde2325f4a32038a8a53780e9f
ds-ai96 <jener05458@gmail.com>
2025-12-23 17:52:56 +0900

    Update script for question07

--- scripts/question07_soft_label.sh
+++ scripts/question07_soft_label.sh
@@ -1,6 +1,6 @@
 gpu=$1
 
-for alpha in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9; do
+for alpha in 0.0 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0; do
 python main_quant.py \
     --gpu "$gpu" \
     --mode DMI \
@@ -9,7 +9,7 @@ python main_quant.py \
     --dataset /home/mjatwk/data/imagenet/ \
     --datapool /home/jener05458/src/EdgeMI/TBD_MI/dataset/ \
     --iterations 4000 \
-    --w_bit 4 \
+    --w_bit 8 \
     --a_bit 8 \
     --seed 0 \
     --use_soft_label \
